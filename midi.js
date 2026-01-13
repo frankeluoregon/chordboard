320f511f8b3ab370666d60220a4ce1b739223fa8
frankeluoregon <123989822+frankeluoregon@users.noreply.github.com>
2026-01-13 12:23:10 -0800

    pitch adjust bass4

--- midi.js
+++ midi.js
@@ -104,19 +104,19 @@ const MIDIPlayer = {
                 'bass-electric': new Tone.Sampler({
                     urls: {
                         // Use electric guitar samples pitched down by mapping to lower octaves
-                        A1: "A2.mp3",
-                        A2: "A3.mp3",
-                        A3: "A4.mp3",
-                        C2: "C3.mp3",
-                        C3: "C4.mp3",
-                        C4: "C5.mp3",
-                        "C#1": "Cs2.mp3",
-                        "D#2": "Ds3.mp3",
-                        "D#3": "Ds4.mp3",
-                        E1: "E2.mp3",
-                        "F#1": "Fs2.mp3",
-                        "F#2": "Fs3.mp3",
-                        "F#3": "Fs4.mp3"
+                        A2: "A2.mp3",
+                        A3: "A3.mp3",
+                        A4: "A4.mp3",
+                        C3: "C3.mp3",
+                        C4: "C4.mp3",
+                        C5: "C5.mp3",
+                        "C#2": "Cs2.mp3",
+                        "D#3": "Ds3.mp3",
+                        "D#4": "Ds4.mp3",
+                        E2: "E2.mp3",
+                        "F#2": "Fs2.mp3",
+                        "F#3": "Fs3.mp3",
+                        "F#4": "Fs4.mp3"
                     },
                     baseUrl: this.baseUrl + "guitar-electric/",
                     release: 1.5, // Longer release for bass character
